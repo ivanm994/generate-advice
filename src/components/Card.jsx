ab264a04a8d85ac7d3aae8950968429027e9917f
Ivan Mijailovic <ivan.mijailovic994@gmail.com>
2023-09-27 22:37:25 +0200

    Set up useEffect hook for api fetch

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import "./card.css";
 import patternDivider from "../assets/pattern-divider.svg";
 import iconDice from "../assets/icon-dice.svg";
+import { useState, useEffect } from "react";
 
 const Card = () => {
+  const URL = "https://api.adviceslip.com/advice";
+  const [content, setContent] = useState(null);
+
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="card">
       <div className="number">#61 Advice</div>
