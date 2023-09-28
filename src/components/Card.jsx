19bfc6caf1682f99f9bddecb947aac652cea6bab
Ivan Mijailovic <ivan.mijailovic994@gmail.com>
2023-09-28 20:52:18 +0200

    Make loading so it has time to load api from server

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,30 +7,39 @@ import { useState, useEffect } from "react";
 const Card = () => {
   const URL = "https://api.adviceslip.com/advice";
   const [content, setContent] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = () => {
+    setIsLoading(true);
     fetch(URL)
       .then((res) => res.json())
       .then((data) => {
         setContent(data.slip);
+        setIsLoading(false);
       })
       .catch((err) => console.error(err));
   };
 
   return (
     <div className="card">
-      <div className="number">{content.id}</div>
-      <div className="advice">{content.advice} </div>
-      <div className="wrapper">
-        <img src={patternDivider} alt="divider image" />
-        <button onClick={fetchData}>
-          <img src={iconDice} alt="dice icon image" />
-        </button>
-      </div>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : content ? (
+        <>
+          <div className="number">{content.id}#</div>
+          <div className="advice">{content.advice} </div>
+          <div className="wrapper">
+            <img src={patternDivider} alt="divider image" />
+            <button onClick={fetchData}>
+              <img src={iconDice} alt="dice icon image" />
+            </button>
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
